7ee16da93bc517824f1918db93994ca669a8c7dd
Max Clarke <reflux444@gmail.com>
2023-03-01 08:34:15 -0500

    first commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
